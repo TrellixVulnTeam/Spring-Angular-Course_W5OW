29d7f164c16331e8ccb2104df650af2e21a43a8a
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-09-19 04:12:15 -0300

    Login Form

--- ponto-inteligente/src/app/autenticacao/login/components/login/login.component.ts
+++ ponto-inteligente/src/app/autenticacao/login/components/login/login.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+//FormBuilder - Ajuda a criar o mapeamento do formulário
+//FormGroup - Agrupar os componentes (no caso email e senha)
+//Validators - Validações
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+//MatSnackBar - Exibir mensagens de erro e sucesso na tela
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  form!: FormGroup;
+  
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private router: Router) { 
   }
 
+  ngOnInit(){
+    this.gerarForm();
+  }
+
+  gerarForm(){
+    this.form = this.fb.group({
+      //O primeiro parâmetro é o valor padrão do campo, no nosso caso deve começar vazio
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  logar(){
+    if(this.form.invalid){
+      this.snackBar.open("Dados inválidos", "Erro", {duration: 5000});
+      return;
+    }
+    alert(JSON.stringify(this.form.value));
+  }
 }
--- ponto-inteligente/src/app/autenticacao/login/login.module.ts
+++ ponto-inteligente/src/app/autenticacao/login/login.module.ts
@@ -2,14 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent, LogarComponent } from './components';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; //Permite a conexão com a API
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule} from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+import { FlexLayoutModule } from '@angular/flex-layout'; //Vai nos ajudar a centralizar alguns elementos
 
 @NgModule({
   declarations: [
-    LoginComponent, LogarComponent
+    LoginComponent, 
+    LogarComponent
   ],
   imports: [
-    CommonModule, RouterModule
+    CommonModule, 
+    RouterModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    FlexLayoutModule
   ]
 })
 export class LoginModule { }
